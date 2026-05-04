eb8434dd5778b7ab41c860e5881e5b0e0c05fd35
Matthew <killerfoxgaming08712@gmail.com>
2026-05-04 14:24:56 -0400

    getting general look of scratch-off ticket

---
+++ index.js
@@ -0,0 +1,9 @@
+function genTicket() {
+   const scratchticket = document.getElementById("scratchoff");
+   for (let i=0;i<5;i++) {
+     const col = document.createElement("div");
+     col.style.display = "flex";
+     col.style.flexDirection = "column";
+     
+   }
+}
